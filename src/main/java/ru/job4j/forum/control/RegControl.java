80c0c58e47942053f84260f4dc0c5c5bca13bf85
AshleySunsine <sgt16@yandex.ru>
2022-03-27 19:04:09 +0300

    2. Spring boot security [#296071 #278053]01

--- src/main/java/ru/job4j/forum/control/RegControl.java
+++ src/main/java/ru/job4j/forum/control/RegControl.java
@@ -1,25 +1,42 @@
 package ru.job4j.forum.control;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import ru.job4j.forum.model.User;
+import ru.job4j.forum.repositories.AuthorityRepository;
 import ru.job4j.forum.repositories.UserRepository;
 
+@Controller
 public class RegControl {
-    private final UserRepository userRepository;
+    @Autowired
+    private final PasswordEncoder encoder;
 
-    public RegControl(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    private final UserRepository users;
+    private final AuthorityRepository authorities;
 
-    @PostMapping("/reg")
-    public String regSave(@ModelAttribute User user) {
-        return "redirect:/login";
+    public RegControl(PasswordEncoder encoder, UserRepository users, AuthorityRepository authorities) {
+        this.encoder = encoder;
+        this.users = users;
+        this.authorities = authorities;
     }
 
     @GetMapping("/reg")
     public String regPage() {
-        return "reg";
+        System.out.println("reg2");
+        return "redirect:/reg";
+    }
+
+    @PostMapping("/reg")
+    public String regSave(@ModelAttribute User user) {
+        System.out.println("reg1");
+        user.setEnabled(true);
+        user.setPassword(encoder.encode(user.getPassword()));
+        user.setAuthority(authorities.findByAuthority("ROLE_USER"));
+        users.save(user);
+        return "redirect:/login";
     }
 }
